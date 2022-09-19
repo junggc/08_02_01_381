ed512b18075e587a2f5b1f0b5c2a9d93932ead8d
kacelab <jung@kacelab.com>
2022-09-19 14:03:03 +0900

    의뢰1,2 병합

---
+++ data/bookdata4.js
@@ -0,0 +1,23 @@
+var unit4Data = new Object();
+unit4Data.info =
+{
+	"pageStart" : "419", "pageEnd" : "456"
+}
+
+unit4Data.listDB =
+{
+	"title" : "1. ",
+	"page" : "419",
+	"children" : [
+		{
+			"title" : "1)", "page" : "419",
+			"children" : [
+				{"title" : "", "page" : "419"}
+			]
+		},
+		{"title" : "1단계 학습평가", "page" : "449"},
+		{"title" : "2단계 학습평가", "page" : "451"},
+		{"title" : "3단계 학습평가", "page" : "453"},
+		{"title" : "내가 실천한 활동을 이야기해봅시다.", "page" : "455"}
+	]
+}
